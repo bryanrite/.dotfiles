fa6c3e405058b2c49ffde4b7b3dee514194f05fd
Bryan Rite <bryan@bryanrite.com>
2016-10-18 09:24:15 +0200

    Colour and add auto features to zsh.

---
+++ system/aliases.zsh
@@ -0,0 +1,18 @@
+# grc overides for ls
+#   Made possible through contributions from generous benefactors like
+#   `brew install coreutils`
+# if $(gls &>/dev/null)
+# then
+#   alias ls="gls -F --color"
+#   alias l="gls -lAh --color"
+#   alias ll="gls -l --color"
+#   alias la='gls -A --color'
+# fi
+
+alias ls="ls -F"
+alias l="ls -lAhp"
+alias ll="ls -l"
+alias la="ls -A"
+
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
---
+++ zsh/plugins.zsh
@@ -0,0 +1,3 @@
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=239"
